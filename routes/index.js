89e4a9978e27e32ba4555dba939dddd5e5ca4c42
Zhen Du <jenkin.du@gmail.com>
2018-10-04 21:07:47 -0400

    get post method

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* Redirect to original url */
+router.get('/:code', function(req, res, next) {
+  res.redirect("http://localhost:3000");
+});
+
+/* Call shortener service */
+router.post('/', function(req, res, next) {
+  var original = req.body.original;
+  var base = req.body.base;
+
+  res.render('index', { title: base + original });
+});
 
 module.exports = router;
