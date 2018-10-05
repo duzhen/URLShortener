d8caa3c6635cf3d5c75920337f3ef232c280d75d
Zhen Du <jenkin.du@gmail.com>
2018-10-04 21:39:16 -0400

    integrate shortid

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+const shortid = require("shortid");
+const validurl = require("valid-url");
 var router = express.Router();
+
+var urlcache = {};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,14 +11,20 @@ router.get('/', function(req, res, next) {
 });
 /* Redirect to original url */
 router.get('/:code', function(req, res, next) {
-  res.redirect("http://localhost:3000");
+  res.redirect(urlcache[code]);
 });
 
 /* Call shortener service */
 router.post('/', function(req, res, next) {
   var original = req.body.original;
   var base = req.body.base;
-
+  if (validurl.isUri(original)) {
+    code = shortid.generate();
+    urlcache[code] = original;
+    res.render('index', { title: base+"/"+code });
+  } else {
+    res.render('index', { title: "invalid url" });
+  }
   res.render('index', { title: base + original });
 });
 
